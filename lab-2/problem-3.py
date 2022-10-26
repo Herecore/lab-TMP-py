eb8a02cea30b26c7ee214dd3df781524a586ae3f
Nicky <92900519+Herecore@users.noreply.github.com>
2022-10-26 11:27:46 +0300

    uploaded problem 3 solution

---
+++ lab-2/problem-3.py
@@ -0,0 +1,9 @@
+n = int(input("Enter n: "))
+m = int(input("Enter m: "))
+
+if n < m:
+    print("n is smaller than m")
+elif m < n:
+    print("m is smaller than n")
+else:
+    print("Numbers are equal")
